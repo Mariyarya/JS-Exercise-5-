551702afe5def8a8b5eb9d53083dccac0022a955
Mariyarya <78618476+Mariyarya@users.noreply.github.com>
2021-07-05 18:34:06 +0300

    - Conditionals

---
+++ ppp.js
@@ -0,0 +1,106 @@
+//--------------1-----------------
+//Write a JavaScript program that accept two integers and display the
+// larger in the console. 
+// const a = 10;
+// const b = 15;
+// if (a >= b) {
+//     console.log('A mai mare')
+// }
+// else {
+//     console.log('B mai mare')
+// }
+
+
+
+
+
+
+
+
+
+//--------------------2----------
+// Write a JavaScript conditional statement to find the sign of product of three numbers. Display the specified sign in the console. 
+// Sample numbers : 3, -7, 2
+// Output : The sign is -
+
+// const a = 3;
+// const b = -7;
+// const c = 2;
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log(`it's +`);
+// }
+
+// else if (a < 0 && b < 0 && c > 0) {
+//     console.log(`it's -`);
+// }
+// else if (a > 0 && b < 0 && c < 0) {
+//     console.log(`it's +`);
+// }
+// else if (a < 0 && b > 0 && c < 0) {
+//     console.log(`it's +`);
+// }
+// else {
+//     console.log(`it's -`); //-
+// }
+
+
+// //--------------------3-------------
+// //  Write a JavaScript conditional statement to sort three numbers. Display the result in the console
+// // Sample numbers : 0, -1, 4
+// // Output : 4, 0, -1 
+
+// const a = 0;
+// const b = -1;
+// const c = 4;
+// if (a > b && a > c) {
+//     if (b > c) {
+//         console.log(a + ", " + b + ", " + c);
+//     }
+//     else {
+//         console.log(a + ", " + c + ", " + b);
+//     }
+// }
+// else if (b > a && y > c) {
+//     if (a > c) {
+//         console.log(b + ", " + a + ", " + c);
+//     }
+//     else {
+//         console.log(b + ", " + c + ", " + a);
+//     }
+// }
+// else if (c > a && c > b) {
+//     if (a > b) {
+//         console.log(c + ", " + a + ", " + b);
+//     }
+//     else {
+//         console.log(c + ", " + b + ", " + a);
+//     }
+// }
+
+
+
+//------------------4---------------
+// Write a JavaScript conditional statement to find the largest of five numbers.Display the result in the console. 
+// Sample numbers : -5, -2, -6, 0, -1
+// Output : 0
+
+// const a = -5;
+// const b = -2;
+// const c = -6;
+// const d = 0;
+// const f = -1;
+// if (a > b && a > c && a > d && a > f) {
+//     console.log(a);
+// }
+// else if (b > a && b > c && b > d && b > f) {
+//     console.log(b);
+// }
+// else if (c > a && c > b && c > d && c > f) {
+//     console.log(c);
+// }
+// else if (d > a && d > c && d > b && d > f) {
+//     console.log(d);
+// }
+// else {
+//     console.log(f);
+// }
